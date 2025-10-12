f73486ede3498e4810c02a1d2d4425e22e5c1263
Akshit Kumar Bansal <158252583+AkshitKumarBansal@users.noreply.github.com>
2025-10-12 13:30:15 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (63.25%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -15,20 +15,25 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
+        // if(root == null) return 0;
+        // List<List<Integer>> res = new ArrayList<>(); // using levelOrder
+        // Queue<TreeNode> queue = new ArrayDeque<>();
+        // queue.offer(root);
+        // while(!queue.isEmpty()) {
+        //     int level = queue.size();
+        //     List<Integer> list = new ArrayList<>();
+        //     for(int i=0;i<level;i++) {
+        //         if(queue.peek().left != null) queue.offer(queue.peek().left);
+        //         if(queue.peek().right != null) queue.offer(queue.peek().right);
+        //         list.add(queue.poll().val);
+        //     }
+        //     res.add(new ArrayList<>(list));
+        // }
+        // return res.size();
+
         if(root == null) return 0;
-        List<List<Integer>> res = new ArrayList<>();
-        Queue<TreeNode> queue = new ArrayDeque<>();
-        queue.offer(root);
-        while(!queue.isEmpty()) {
-            int level = queue.size();
-            List<Integer> list = new ArrayList<>();
-            for(int i=0;i<level;i++) {
-                if(queue.peek().left != null) queue.offer(queue.peek().left);
-                if(queue.peek().right != null) queue.offer(queue.peek().right);
-                list.add(queue.poll().val);
-            }
-            res.add(new ArrayList<>(list));
-        }
-        return res.size();
+        int lh = maxDepth(root.left);
+        int rh = maxDepth(root.right);
+        return 1+Math.max(lh, rh);
     }
 }
