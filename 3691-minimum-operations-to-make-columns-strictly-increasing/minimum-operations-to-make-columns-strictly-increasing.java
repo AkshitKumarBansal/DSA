b04a5b37c69c6b57b39a1e6448eb839e551dc241
Akshit Kumar Bansal <158252583+AkshitKumarBansal@users.noreply.github.com>
2025-11-13 18:43:16 +0530

    Time: 1 ms (100.00%) | Memory: 46.7 MB (18.78%) - LeetSync

---
+++ 3691-minimum-operations-to-make-columns-strictly-increasing/minimum-operations-to-make-columns-strictly-increasing.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minimumOperations(int[][] grid) {
+        int count = 0;
+        for(int j=0;j<grid[0].length;j++) {
+            for(int i=1;i<grid.length;i++) {
+                if(grid[i-1][j] >= grid[i][j]) {
+                    count += grid[i-1][j]-grid[i][j]+1;
+                    grid[i][j] = grid[i-1][j]+1;
+                } 
+            }
+        }
+        return count;
+    }
+}
