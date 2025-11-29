9e71cdadf654213f32346a3fd208ffd1f0ff77c3
Akshit Kumar Bansal <158252583+AkshitKumarBansal@users.noreply.github.com>
2025-11-29 19:16:49 +0530

    Time: 16 ms (86.98%) | Memory: 45.6 MB (47.59%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -7,12 +7,12 @@ class Solution {
             for(int j=1;j<=n;j++) {
                 if(s1.charAt(i-1)==s2.charAt(j-1)) {
                     dp[i][j] = (int)s1.charAt(i-1) + dp[i-1][j-1];
-                    if(i>1) {
-                        dp[i][j] = Math.max(dp[i][j], dp[i-1][j]);
-                    }
-                    if(j>1) {
-                        dp[i][j] = Math.max(dp[i][j], dp[i][j-1]);
-                    }
+                    // if(i>1) {
+                    //     dp[i][j] = Math.max(dp[i][j], dp[i-1][j]);
+                    // }
+                    // if(j>1) {
+                    //     dp[i][j] = Math.max(dp[i][j], dp[i][j-1]);
+                    // }
                 } else {
                     dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 }
