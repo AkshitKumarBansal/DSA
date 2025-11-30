c1dd5dfead7e33431bb6c8c4011e5b18deec38a7
Akshit Kumar Bansal <158252583+AkshitKumarBansal@users.noreply.github.com>
2025-11-30 20:12:13 +0530

    Time: 1 ms (89.83%) | Memory: 43 MB (37.40%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
@@ -4,7 +4,6 @@ class Solution {
         int i = 0;
         int j = 1;
         int k = 2;
-        if(s.length()==2) return (s.charAt(i)!=s.charAt(j)) ? 2 : 1;
         int cnt = 0;
         while(k < s.length()) {
             if(s.charAt(i)!=s.charAt(j) && s.charAt(i)!=s.charAt(k) && s.charAt(j)!=s.charAt(k)) {
