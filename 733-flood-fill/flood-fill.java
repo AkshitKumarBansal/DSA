79883123afdd2e735184fcc7a293472387cb15b1
Akshit Kumar Bansal <158252583+AkshitKumarBansal@users.noreply.github.com>
2025-12-17 19:59:07 +0530

    Time: 1 ms (44.14%) | Memory: 47 MB (14.19%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,20 +1,21 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int[][] ans = new int[image.length][image[0].length];
-        for(int i=0;i<image.length;i++) {
-            ans[i] = image[i].clone();
-        }
-        int[] row = {-1,0,1,0};
-        int[] col = {0,1,0,-1};
-        dfs(image, ans, row, col, sr, sc, image[sr][sc], color);
-        return ans;
+        int C = image[sr][sc];
+        int m = image.length;
+        int n = image[0].length;
+        int[][] arr = new int[m][n];
+        dfs(sr, sc, arr, image, color, C, m, n);
+        return image;
     }
-    private void dfs(int[][] grid, int[][] ans, int[] row, int[] col, int i, int j, int color, int newColor) {
-        ans[i][j] = newColor;
-        for(int k=0;k<4;k++) {
-            if(i+row[k]>=0 && i+row[k]<grid.length && j+col[k]>=0 && j+col[k]<grid[0].length && grid[i+row[k]][j+col[k]]==color && ans[i+row[k]][j+col[k]]!=newColor) {
-                dfs(grid, ans, row, col, i+row[k], j+col[k], color, newColor);
-            }
+    private void dfs(int i, int j, int[][] arr, int[][] image, int newC, int C, int m, int n) {
+        if(i<0 || j<0 || i>=m || j>=n || image[i][j]!=C || arr[i][j]==1) return;
+        if(image[i][j]==C) {
+            image[i][j] = newC;
         }
+        arr[i][j] = 1;
+        dfs(i+1, j, arr, image, newC, C, m, n);
+        dfs(i-1, j, arr, image, newC, C, m, n);
+        dfs(i, j+1, arr, image, newC, C, m, n);
+        dfs(i, j-1, arr, image, newC, C, m, n);
     }
 }
