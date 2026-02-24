8dc671ed862c63fdfe398286af4895d7a332c826
Akshit Kumar Bansal <158252583+AkshitKumarBansal@users.noreply.github.com>
2026-02-25 02:21:50 +0530

    Time: 43 ms (75.00%) | Memory: 52 MB (91.48%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -4,22 +4,19 @@ class Solution {
         int MOD = 1_000_000_007;
         int n = arr.length;
         Stack<Integer> st = new Stack<>();
-        long[] ans = new long[n];
-        for(int i=0;i<n;i++) {
-            while(!st.isEmpty() && arr[i]<arr[st.peek()]) st.pop();
-            if(st.isEmpty()) ans[i] = i+1;
-            else ans[i] = i-st.peek();
+        for(int i=0;i<=n;i++) {
+            int curr = (i == n) ? 0 : arr[i];
+            while(!st.isEmpty() && curr<arr[st.peek()]) {
+                int mid = st.pop();
+                int left = st.isEmpty() ? -1 : st.peek();
+                int right = i;
+
+                long leftCount = mid - left;
+                long rightCount = right - mid;
+
+                sum = (sum + arr[mid] * leftCount * rightCount) % MOD;
+            }
             st.push(i);
-        }
-        while(!st.isEmpty()) st.pop();
-        for(int i=n-1;i>=0;i--) {
-            while(!st.isEmpty() && arr[i]<=arr[st.peek()]) st.pop();
-            if(st.isEmpty()) ans[i] *= (n-i);
-            else ans[i] *= (st.peek()-i);
-            st.push(i);
-        }
-        for(int i=0;i<n;i++) {
-            sum = (sum + (long)(arr[i]*ans[i])) % MOD;
         }
         return (int)(sum % MOD);
     }
