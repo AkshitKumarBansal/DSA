3135e91ed42639edd00e81c5e202331d3d3397c6
Akshit Kumar Bansal <158252583+AkshitKumarBansal@users.noreply.github.com>
2026-02-25 02:18:29 +0530

    Time: 63 ms (51.28%) | Memory: 59.1 MB (27.92%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -3,28 +3,21 @@ class Solution {
         long sum = 0;
         int MOD = 1_000_000_007;
         int n = arr.length;
-        Stack<Integer> st1 = new Stack<>();
-        Stack<Integer> st2 = new Stack<>();
+        Stack<Integer> st = new Stack<>();
         long[] ans = new long[n];
         for(int i=0;i<n;i++) {
-            while(!st1.isEmpty() && arr[i]<arr[st1.peek()]) st1.pop();
-            if(st1.isEmpty()) ans[i] = i+1;
-            else ans[i] = i-st1.peek();
-            st1.push(i);
+            while(!st.isEmpty() && arr[i]<arr[st.peek()]) st.pop();
+            if(st.isEmpty()) ans[i] = i+1;
+            else ans[i] = i-st.peek();
+            st.push(i);
         }
-        // for(int i=0;i<n;i++) {
-        //     System.out.print(left[i]+"-");
-        // }
-        // System.out.println();
+        while(!st.isEmpty()) st.pop();
         for(int i=n-1;i>=0;i--) {
-            while(!st2.isEmpty() && arr[i]<=arr[st2.peek()]) st2.pop();
-            if(st2.isEmpty()) ans[i] *= (n-i);
-            else ans[i] *= (st2.peek()-i);
-            st2.push(i);
+            while(!st.isEmpty() && arr[i]<=arr[st.peek()]) st.pop();
+            if(st.isEmpty()) ans[i] *= (n-i);
+            else ans[i] *= (st.peek()-i);
+            st.push(i);
         }
-        // for(int i=0;i<n;i++) {
-        //     System.out.print(right[i]+"-");
-        // }
         for(int i=0;i<n;i++) {
             sum = (sum + (long)(arr[i]*ans[i])) % MOD;
         }
