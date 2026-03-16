eae12a0c55ad9d85e841b0e1437f1b2a2219e467
Akshit Kumar Bansal <158252583+AkshitKumarBansal@users.noreply.github.com>
2026-03-16 16:51:58 +0530

    Time: 5 ms (32.06%) | Memory: 43.9 MB (28.85%) - LeetSync

--- 299-bulls-and-cows/bulls-and-cows.java
+++ 299-bulls-and-cows/bulls-and-cows.java
@@ -2,27 +2,16 @@ class Solution {
     public String getHint(String secret, String guess) {
         int A = 0;
         int B = 0;
-        int[] freq1 = new int[10];
-        int[] freq2 = new int[10];
-        int i=0, j=0;
-        while(i<secret.length() && j<guess.length()) {
-            if(secret.charAt(i)==guess.charAt(j)) A++;
-            freq1[secret.charAt(i)-'0']++;
-            freq2[guess.charAt(j)-'0']++;
-            i++;
-            j++;
+        int[] freq = new int[10];
+        for(int i=0;i<secret.length();i++) {
+            if(secret.charAt(i)==guess.charAt(i)) A++;
+            else {
+                if(freq[secret.charAt(i)-'0']<0) B++;
+                if(freq[guess.charAt(i)-'0']>0) B++;
+                freq[secret.charAt(i)-'0']++;
+                freq[guess.charAt(i)-'0']--;
+            }
         }
-        while(i<secret.length()) {
-            freq1[secret.charAt(i)-'0']++;
-            i++;
-        }
-        while(j<guess.length()) {
-            freq2[guess.charAt(j)-'0']++;
-            j++;
-        }
-        for(int k=0;k<10;k++) {
-            B += Math.min(freq1[k], freq2[k]);
-        }
-        return A+"A"+(B-A)+"B";
+        return A+"A"+B+"B";
     }
 }
