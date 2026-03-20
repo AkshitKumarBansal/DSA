1dd8047960e2a9401cd6346e98832ae7160305cc
Akshit Kumar Bansal <158252583+AkshitKumarBansal@users.noreply.github.com>
2026-03-21 03:32:15 +0530

    Time: 1 ms (95.18%) | Memory: 43.6 MB (28.04%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,27 +1,18 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> list1 = new ArrayList<>();
-        list1.add(1);
-        res.add(new ArrayList<>(list1));
-        if (numRows == 1) {
-            return res;
-        }
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        result.add(new ArrayList<>(list));
         for(int i=1;i<numRows;i++) {
-            List<Integer> list2 = new ArrayList<>();
+            List<Integer> res = new ArrayList<>();
             for(int j=0;j<=i;j++) {
-                if(j == i) {
-                    list2.add(list1.get(j-1));
-                } else if(j == 0) {
-                    list2.add(list1.get(j));
-                } else {
-                    list2.add(list1.get(j)+list1.get(j-1));
-                }
+                if(j==0 || j==i) res.add(1);
+                else res.add(list.get(j-1)+list.get(j));
             }
-            res.add(new ArrayList<>(list2));
-            list1.clear();
-            list1.addAll(list2);
+            list = new ArrayList<>(res);
+            result.add(new ArrayList<>(list));
         }
-        return res;
+        return result;
     }
 }
