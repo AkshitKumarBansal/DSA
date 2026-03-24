1e09215592a4252e2a9edb8f751c5c2e46e31107
Akshit Kumar Bansal <158252583+AkshitKumarBansal@users.noreply.github.com>
2026-03-25 02:48:27 +0530

    Time: 2 ms (99.82%) | Memory: 53.8 MB (43.85%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,20 +1,13 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        int N = trust.length;
-        if(n==1) return 1;
-        if(N==0) return -1;
-        Set<Integer> set = new HashSet<>();
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i=0;i<N;i++) set.add(trust[i][0]);
-        int x = -1;
-        for(int i=1;i<=n;i++) if(!set.contains(i)) x = i;
-        for(int i=0;i<N;i++) {
-            if(!map.containsKey(trust[i][0])) map.put(trust[i][0], trust[i][1]);
-            else {
-                if(map.get(trust[i][0])!=x) map.put(trust[i][0], trust[i][1]);
-            }
+        int[] arr = new int[n+1];
+        for(int[] x : trust) {
+            arr[x[0]]--;
+            arr[x[1]]++;
         }
-        for(int i : map.values()) if(i!=x) return -1;
-        return x;
+        for(int i=1;i<=n;i++) {
+            if(arr[i]==n-1) return i;
+        }
+        return -1;
     }
 }
