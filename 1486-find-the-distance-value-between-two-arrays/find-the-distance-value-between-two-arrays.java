3bad57c8e4b1ce15e329e4b383d5ee788ccf43f7
Akshit Kumar Bansal <158252583+AkshitKumarBansal@users.noreply.github.com>
2026-04-01 16:59:09 +0530

    Time: 10 ms (19.36%) | Memory: 46.9 MB (7.42%) - LeetSync

--- 1486-find-the-distance-value-between-two-arrays/find-the-distance-value-between-two-arrays.java
+++ 1486-find-the-distance-value-between-two-arrays/find-the-distance-value-between-two-arrays.java
@@ -1,11 +1,18 @@
 class Solution {
     public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
         int count = 0;
+        Arrays.sort(arr2);
         int n1 = arr1.length, n2 = arr2.length;
         for(int i=0;i<n1;i++) {
             boolean flag = true;
-            for(int j=0;j<n2;j++) {
-                if(Math.abs(arr2[j]-arr1[i])<=d) flag = false;
+            int l = 0, r = n2-1;
+            while(l<=r) {
+                int mid = (r+l)/2;
+                if(Math.abs(arr2[mid]-arr1[i])<=d) {
+                    flag = false;
+                    break;
+                } else if(arr2[mid]<arr1[i]) l = mid+1;
+                else r = mid-1;
             }
             if(flag) count++;
         }
