8aa7d2dbf5ea1163f3867e1ef8e7d684ddb709b2
Akshit Kumar Bansal <158252583+AkshitKumarBansal@users.noreply.github.com>
2026-04-10 22:48:18 +0530

    Time: 54 ms (5.86%) | Memory: 48.8 MB (5.50%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,17 +1,27 @@
 class Solution {
+    Integer[][] dp;
     public int coinChange(int[] coins, int amount) {
-        int[][] dp = new int[coins.length+1][amount+1];
-        for(int i=0;i<=coins.length;i++) dp[i][0] = 0;
-        for(int j=1;j<=amount;j++) dp[0][j] = (int)1e9;
-        for(int i=1;i<=coins.length;i++) {
-            for(int j=1;j<=amount;j++) {
-                if(coins[i-1]<=j) {
-                    dp[i][j] = Math.min(dp[i-1][j], 1 + dp[i][j-coins[i-1]]);
-                } else {
-                    dp[i][j] = dp[i-1][j];
-                }
+        if(amount == 0) return 0;
+        dp = new Integer[coins.length+1][amount+1];
+        int result = solve(0, coins, amount);
+        return (result==Integer.MAX_VALUE) ? -1 : result;
+    }
+    private int solve(int ind, int[] coins, int amount) {
+        if(amount==0) return dp[ind][amount] = 0;
+        if(ind == coins.length) return Integer.MAX_VALUE;
+        if(dp[ind][amount] != null) return dp[ind][amount];
+        int noPick = solve(ind+1, coins, amount);
+        int pickWait = Integer.MAX_VALUE, pickNext = Integer.MAX_VALUE;
+        if(coins[ind]<=amount) {
+            int res1 = solve(ind, coins, amount - coins[ind]);
+            if(res1 != Integer.MAX_VALUE) {
+                pickWait = 1 + res1;
+            }
+            int res2 = solve(ind+1, coins, amount - coins[ind]);
+            if(res2 != Integer.MAX_VALUE) {
+                pickNext = 1 + res2;
             }
         }
-        return (dp[coins.length][amount]>=(int)1e9) ? -1 : dp[coins.length][amount];
+        return dp[ind][amount] = Math.min(noPick, Math.min(pickWait, pickNext));
     }
 }
