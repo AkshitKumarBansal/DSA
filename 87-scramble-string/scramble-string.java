065166689263ba2d5d7a2ffb6e8b069a347edb73
Akshit Kumar Bansal <158252583+AkshitKumarBansal@users.noreply.github.com>
2026-04-16 02:57:14 +0530

    Time: 5 ms (88.47%) | Memory: 46.3 MB (77.85%) - LeetSync

--- 87-scramble-string/scramble-string.java
+++ 87-scramble-string/scramble-string.java
@@ -1,26 +1,32 @@
 class Solution {
-    Map<String, Boolean> map;
+    Boolean[][][] dp;
     public boolean isScramble(String s1, String s2) {
-        map = new HashMap<>();
-        return solve(s1, s2);
-    }
-    private boolean solve(String s1, String s2) {
         int n = s1.length();
-        if(s1.equals(s2)) return true;
-        String key = s1+" "+s2;
-        if(map.containsKey(key)) return map.get(key);
-        boolean flag = false;
-        for(int i=1;i<n;i++) {
-            boolean flag1 = false;
-            boolean flag2 = false;
-            if(solve(s1.substring(0,i), s2.substring(0,i)) && solve(s1.substring(i,n), s2.substring(i,n))) flag1 = true;
-            if(solve(s1.substring(0,i), s2.substring(n-i,n)) && solve(s1.substring(i,n), s2.substring(0,n-i))) flag2 = true;
-            if(flag1 || flag2) {
+        dp = new Boolean[n][n][n+1];
+        return solve(s1, s2, 0, 0, n);
+    }
+    private boolean solve(String s1, String s2, int i, int j, int len) {
+        if(dp[i][j][len] != null) return dp[i][j][len];
+        boolean flag = true;
+        for (int k = 0; k < len; k++) {
+            if (s1.charAt(i + k) != s2.charAt(j + k)) {
+                flag = false;
+                break;
+            }
+        }
+        if(flag) dp[i][j][len] = true;
+        for(int k=1;k<len;k++) {
+            if (solve(s1, s2, i, j, k) &&
+                solve(s1, s2, i + k, j + k, len - k)) {
+                flag = true;
+                break;
+            }
+            if (solve(s1, s2, i, j + len - k, k) &&
+                solve(s1, s2, i + k, j, len - k)) {
                 flag = true;
                 break;
             }
         }
-        map.put(key, flag);
-        return flag;
+        return dp[i][j][len] = flag;
     }
 }
