e496d27a16b5f3d31dcce816906d47c1f7b0002e
Akshit Kumar Bansal <158252583+AkshitKumarBansal@users.noreply.github.com>
2026-04-15 23:53:31 +0530

    Time: 112 ms (12.60%) | Memory: 47.5 MB (38.92%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,39 +1,73 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(s.length() < t.length()) return "";
-        Map<Character, Integer> map1 = new HashMap<>();
-        Map<Character, Integer> map2 = new HashMap<>();
-        for(int i=0;i<t.length();i++) {
-            map1.put(t.charAt(i), map1.getOrDefault(t.charAt(i), 0)+1);
+        int i=0;
+        int n = s.length();
+        int min = Integer.MAX_VALUE;
+        String res = "";
+        int freq1[] = new int[26];
+        int freq2[] = new int[26];
+        for(int k = 0; k < t.length(); k++) {
+            char ch = t.charAt(k);
+            if(ch>='a' && ch<='z') freq1[ch-'a']++;
+            else freq2[ch-'A']++;
         }
-        int i = 0;
-        int j = 0;
-        int min = s.length()+1;
-        int x = 0;
-        int y = 0;
-        int count = 0;
-        while(j<s.length()) {
-            if(map1.containsKey(s.charAt(j))) {
-                map2.put(s.charAt(j), map2.getOrDefault(s.charAt(j), 0)+1);
-                if(map2.get(s.charAt(j)).intValue()==map1.get(s.charAt(j)).intValue()) {
-                    count++;
-                }
+        for(int j=0;j<n;j++) {
+            char ch = s.charAt(j);
+            if(ch>='a' && ch<='z') {
+                freq1[ch-'a']--;
+            } else {
+                freq2[ch-'A']--;
             }
-            j++;
-            while(map1.size()==count) {
-                if(min > j-i) {
-                    min = Math.min(min, j-i);
-                    x = i;
+            boolean flag = true;
+            for(int k=0;k<26;k++) {
+                if(freq1[k]>0) flag = false;
+                if(freq2[k]>0) flag = false;
+            }
+            while(flag) {
+                char c = s.charAt(i);
+                if(c>='a' && c<='z') {
+                    freq1[c-'a']++;
+                } else {
+                    freq2[c-'A']++;
                 }
-                if(map1.containsKey(s.charAt(i))) {
-                    if(map2.get(s.charAt(i)).intValue()==map1.get(s.charAt(i)).intValue()) {
-                        count--;
-                    }
-                    map2.put(s.charAt(i), map2.getOrDefault(s.charAt(i), 0)-1);
+                if(min>(j-i+1)) {
+                    min = j-i+1;
+                    res = s.substring(i, j+1);
                 }
                 i++;
+                flag = true;
+                for(int k=0;k<26;k++) {
+                    if(freq1[k]>0) flag = false;
+                    if(freq2[k]>0) flag = false;
+                }
             }
         }
-        return (min == s.length()+1) ? "" : s.substring(x, x + min);
+        return res;
     }
+    // private boolean check(String s, String t) {
+    //     int freq1[] = new int[26];
+    //     int freq2[] = new int[26];
+    //     for(int i=0;i<t.length();i++) {
+    //         char ch = t.charAt(i);
+    //         if(ch>='a' && ch<='z') freq1[ch-'a']++;
+    //         else freq2[ch-'A']++;
+    //     }
+    //     for(int i=0;i<s.length();i++) {
+    //         char ch = s.charAt(i);
+    //         if(ch>='a' && ch<='z') {
+    //             if(freq1[ch-'a']!=0) {
+    //                 freq1[s.charAt(i)-'a']--;
+    //             }
+    //         } else {
+    //             if(freq2[ch-'A']!=0) {
+    //                 freq2[s.charAt(i)-'A']--;
+    //             }
+    //         }
+    //     }
+    //     for(int i=0;i<26;i++) {
+    //         if(freq1[i]>0) return false;
+    //         if(freq2[i]>0) return false;
+    //     }
+    //     return true;
+    // }
 }
