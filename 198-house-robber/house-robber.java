8e0114f1ac143c29d5552bd9adbc7f7e8f665de0
Akshit Kumar Bansal <158252583+AkshitKumarBansal@users.noreply.github.com>
2026-04-23 17:01:54 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (6.90%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,19 +1,11 @@
 class Solution {
-    Integer[][] dp;
     public int rob(int[] nums) {
         int n = nums.length;
-        dp = new Integer[n+1][2];
-        return solve(nums, 0, 0);
-    }
-    private int solve(int[] arr, int ind, int rob) {
-        if(ind == arr.length) return 0;
-        if(dp[ind][rob]!=null) return dp[ind][rob];
-        int cash = 0;
-        if(rob == 0) {
-            cash = Math.max(arr[ind]+solve(arr, ind+1, 1), solve(arr, ind+1, 0));
-        } else {
-            cash = solve(arr, ind+1, 0);
+        int[][] dp = new int[n+1][2];
+        for(int i=n-1;i>=0;i--) {
+            dp[i][0] = Math.max(nums[i]+dp[i+1][1], dp[i+1][0]);
+            dp[i][1] = dp[i+1][0];
         }
-        return dp[ind][rob] = cash;
+        return dp[0][0];
     }
 }
