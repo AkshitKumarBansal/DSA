5c1e07326ba65b6646fd645e92cc40adf2a9b6c2
Akshit Kumar Bansal <158252583+AkshitKumarBansal@users.noreply.github.com>
2026-04-23 23:51:03 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (43.54%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -2,29 +2,20 @@ class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         if(n==1) return nums[0];
-        int[][] dp = new int[n+1][2];
+        int pick1 = 0, noPick1 = 0;
         for(int i=n-1;i>0;i--) {
-            dp[i][0] = Math.max(nums[i]+dp[i+1][1], dp[i+1][0]);
-            dp[i][1] = dp[i+1][0];
+            int newPick1 = Math.max(nums[i]+noPick1, pick1);
+            int newNoPick1 = pick1;
+            pick1 =  newPick1;
+            noPick1 = newNoPick1;
         }
-        int ans1 = dp[1][0];
-        dp = new int[n][2];
+        int pick2 = 0, noPick2 = 0;
         for(int i=n-2;i>=0;i--) {
-            dp[i][0] = Math.max(nums[i]+dp[i+1][1], dp[i+1][0]);
-            dp[i][1] = dp[i+1][0];
+            int newPick2 = Math.max(nums[i]+noPick2, pick2);
+            int newNoPick2 = pick2;
+            pick2 =  newPick2;
+            noPick2 = newNoPick2;
         }
-        int ans2 = dp[0][0];
-        return Math.max(ans1, ans2);
+        return Math.max(pick1, pick2);
     }
-    // private int solve(int[] arr, int ind, int rob, int end) {
-    //     if(ind==end) return 0;
-    //     if(dp[ind][rob]!=null) return dp[ind][rob];
-    //     int cash = 0;
-    //     if(rob==0) {
-    //         cash = Math.max(arr[ind]+solve(arr, ind+1, 1, end), solve(arr, ind+1, 0, end));
-    //     } else {
-    //         cash = solve(arr, ind+1, 0, end);
-    //     }
-    //     return dp[ind][rob] = cash;
-    // }
 }
