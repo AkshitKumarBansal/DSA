5def2a510054f9ec9f19d875c94577a156abf7ed
Akshit Kumar Bansal <158252583+AkshitKumarBansal@users.noreply.github.com>
2026-04-27 23:23:42 +0530

    Time: 2 ms (86.39%) | Memory: 47.5 MB (95.10%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        // if(nums.length <= 1) return true;
         int max = 0;
         for(int i=0;i<nums.length;i++) {
             if(max >= nums.length-1) return true;
