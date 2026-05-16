d6c371837ff3c58e2287b0fc73c70cf4212f0564
Akshit Kumar Bansal <158252583+AkshitKumarBansal@users.noreply.github.com>
2026-05-16 18:29:04 +0530

    Time: 1 ms (100.00%) | Memory: 47 MB (55.81%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,23 +1,15 @@
 class Solution {
     public int[] asteroidCollision(int[] arr) {
-        Stack<Integer> st = new Stack<>();
-        int n = arr.length;
-        for(int i=0;i<n;i++) {
-            if(arr[i] < 0) {
-                while(!st.isEmpty() && st.peek()>0 && st.peek()<Math.abs(arr[i])) {
-                    st.pop();
-                }
-                if(st.isEmpty() || (!st.isEmpty() && st.peek()<0)) st.push(arr[i]);
-                if(!st.isEmpty() && st.peek()==Math.abs(arr[i])) st.pop();
+        int topIndex = -1;
+        for(int a : arr) {
+            if(a < 0) {
+                while(topIndex >= 0 && arr[topIndex] > 0 && arr[topIndex] < -a) topIndex--;
+                if(topIndex < 0 || (topIndex >= 0 && arr[topIndex] < 0)) arr[++topIndex] = a;
+                if(topIndex>=0 && arr[topIndex] == -a) topIndex--;
             } else {
-                st.push(arr[i]);
+                arr[++topIndex] = a;
             }
         }
-        int[] result = new int[st.size()];
-        int i = st.size()-1;
-        while(!st.isEmpty()) {
-            result[i--] = st.pop();
-        }
-        return result;
+        return Arrays.copyOfRange(arr, 0, topIndex+1);
     }
 }
