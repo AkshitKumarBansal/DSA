40f4e21b6f2faa54acfc99ff68e7dc4ff922c0e5
Akshit Kumar Bansal <158252583+AkshitKumarBansal@users.noreply.github.com>
2026-07-30 18:32:15 +0530

    Time: 109 ms (19.64%) | Memory: 76.2 MB (92.32%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,27 +1,25 @@
 class Solution {
-    // Create two arrays one prevSmallest another nextSmallest store the index of prev and next smallest elements in them.
-    public int largestRectangleArea(int[] arr) {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
         Stack<Integer> st = new Stack<>();
-        int n = arr.length;
         int[] prev = new int[n];
-        for(int i=0;i<n;i++) {
-            while(!st.isEmpty() && arr[st.peek()]>=arr[i]) st.pop();
+        for(int i = 0; i < n; i++) {
+            while(!st.isEmpty() && heights[st.peek()] >= heights[i]) st.pop();
             if(st.isEmpty()) prev[i] = -1;
             else prev[i] = st.peek();
             st.push(i);
         }
-        while(!st.isEmpty()) st.pop();
+        st.clear();
         int[] next = new int[n];
-        for(int i=n-1;i>=0;i--) {
-            while(!st.isEmpty() && arr[st.peek()]>=arr[i]) st.pop();
+        for(int i = n-1; i >= 0; i--) {
+            while(!st.isEmpty() && heights[st.peek()] >= heights[i]) st.pop();
             if(st.isEmpty()) next[i] = n;
             else next[i] = st.peek();
             st.push(i);
         }
         int max = 0;
-        for(int i=0;i<n;i++) {
-            int area = arr[i]*(next[i]-prev[i]-1); // Formula to calculate the maximum area for each index
-            max = Math.max(area, max);
+        for(int i = 0; i < n; i++) {
+            max = Math.max((next[i]-prev[i]-1) * heights[i], max);
         }
         return max;
     }
